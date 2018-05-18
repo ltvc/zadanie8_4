7f273ecd8998225233e299f090dcf4b8c3d7da60
ltvc <lataviec666@gmail.com>
2018-05-18 11:28:55 +0200

    Add js file

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var imie = prompt('Wpisz swoje imie');
+alert('Czesc, ' + imie); // komentarz liniowy
+console.log('Czesc, ' + imie); //podaj swoje imie i podstaw do zmiennej imie
+/*
+  komentarz blokowy
+  console.log('The commented will not be called');
+*/
